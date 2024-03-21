de52136d32ffb0014ef579ef2b9378d85f7fd2c0
suleka123 <sulekavinodani@gmail.com>
2024-03-21 17:22:14 +0100

    new correction

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Job;
+use App\Entity\Company;
 
-class JobController extends AbstractController
+use App\Repository\JobRepository;
+use App\Repository\JobTypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
 {
-    #[Route('/job', name: 'app_job')]
-    public function job(): Response
+    #[Route('/jobs', name: 'app_jobs')]
+    public function index(JobRepository $movieRepository, Request $request): Response
     {
+        $genreId = $request->get('genreId');
+        $jobs = $jobRepository->findjobs($genreId);
+
         return $this->render('job/index.html.twig', [
-            'controller_name' => 'JobController',
+            'jobs' => $jobs,
         ]);
     }
+
+    #[Route('/jobs/{id}', name: 'app_job_show')]
+    public function show(job $job, Request $request, EntityManagerInterface $entityManager, 
+                        Security $security, ReviewRepository $reviewRepository, SessionInterface $session): Response
+       {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
+        ]);
+       }
+
 }
+                  
+        
