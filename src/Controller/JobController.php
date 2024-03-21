d8c431cef98ca5b5255410e91be03c5c945b1d2f
suleka123 <sulekavinodani@gmail.com>
2024-03-21 17:24:24 +0100

    Job page

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -15,13 +15,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MovieController extends AbstractController
+class JobController extends AbstractController
 {
     #[Route('/jobs', name: 'app_jobs')]
-    public function index(JobRepository $movieRepository, Request $request): Response
+    public function index(JobRepository $jobRepository, Request $request): Response
     {
-        $genreId = $request->get('genreId');
-        $jobs = $jobRepository->findjobs($genreId);
+        $jobs = $jobRepository->findAll();
 
         return $this->render('job/index.html.twig', [
             'jobs' => $jobs,
@@ -29,8 +28,8 @@ class MovieController extends AbstractController
     }
 
     #[Route('/jobs/{id}', name: 'app_job_show')]
-    public function show(job $job, Request $request, EntityManagerInterface $entityManager, 
-                        Security $security, ReviewRepository $reviewRepository, SessionInterface $session): Response
+    public function show(Job $job, Request $request, EntityManagerInterface $entityManager, 
+                        Security $security, SessionInterface $session): Response
        {
         return $this->render('job/show.html.twig', [
             'job' => $job,
